2f13a87b29dc5341f9da69e29c03b7cc93f755a1
lza_menace <lza_menace@protonmail.com>
2024-10-18 20:52:14 -0700

    fix indentation

--- api/lib/mempool.py
+++ api/lib/mempool.py
@@ -10,37 +10,37 @@ from api.helpers import log, bcolors
 redis = Redis()
 
 class Mempool(object):
-  def __init__(self):
-    self.chain = secrets["CHAIN"]
-    if self.chain == "mainnet":
-      self.url = "https://mempool.space/api/v1"
-    elif self.chain == "testnet":
-      self.url = "https://mempool.space/testnet/api/v1"
-    elif self.chain == "regtest":
-      pass
-    else:
-      raise Exception(f"CHAIN must be mainnet, regtest, or testnet, you specified \"{self.chain}\"")
+    def __init__(self):
+        self.chain = secrets["CHAIN"]
+        if self.chain == "mainnet":
+            self.url = "https://mempool.space/api/v1"
+        elif self.chain == "testnet":
+            self.url = "https://mempool.space/testnet/api/v1"
+        elif self.chain == "regtest":
+            pass
+        else:
+            raise Exception(f"CHAIN must be mainnet, regtest, or testnet, you specified \"{self.chain}\"")
 
-  def get_fees(self):
-    if self.chain == "regtest":
-      return {"fastestFee": 2}
-    url = self.url + "/fees/recommended"
-    key_name = "fees"
-    data = redis.get(key_name)
-    if data:
-      log("cache hit", bcolors.OKGREEN)
-      return json.loads(data)
-    else:
-      log("cache miss", bcolors.FAIL)
-      try:
-        res = requests.get(url)
-        res.raise_for_status()
-        redis.setex(
-          name=key_name,
-          time=600,
-          value=json.dumps(res.json())
-        )
-        return res.json()
-      except Exception as e:
-        print(e)
-        return {}
+    def get_fees(self):
+        if self.chain == "regtest":
+            return {"fastestFee": 2}
+        url = self.url + "/fees/recommended"
+        key_name = "fees"
+        data = redis.get(key_name)
+        if data:
+            log("cache hit", bcolors.OKGREEN)
+            return json.loads(data)
+        else:
+            log("cache miss", bcolors.FAIL)
+            try:
+                res = requests.get(url)
+                res.raise_for_status()
+                redis.setex(
+                    name=key_name,
+                    time=600,
+                    value=json.dumps(res.json())
+                )
+                return res.json()
+            except Exception as e:
+                log(e, bcolors.FAIL)
+                return {}
